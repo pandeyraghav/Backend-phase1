68309ed1ffd0b5d02a783e302bf326a5c3dc8b6f
pandeyraghav <raghavpandey2022@gmail.com>
2025-10-11 15:42:00 +0530

    Added addTask function

---
+++ todo/todo.js
@@ -0,0 +1,39 @@
+const { log } = require('console');
+const fs = require('fs');
+const filePath = "./tasks.json";
+
+const loadTasks = () => {
+    try {
+        const dataBuffer = fs.readFileSync(filePath);
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (error) {
+        return []
+    }
+}
+
+const saveTasks = (tasks) => {
+    const dataJSON = JSON.stringify(tasks);
+    fs.writeFileSync(filePath, dataJSON);
+    log("Task saved successfully !");
+}
+
+const addTask = (task) => {
+    const tasks = loadTasks()
+    tasks.push(task)
+    saveTasks(tasks)
+}
+
+const command = process.argv[2];
+const argument = process.argv[3];
+
+
+if(command === 'add'){
+    addTask(argument)
+}else if(command === 'list'){
+    listTask()
+}else if(command === 'remove'){
+    removeTask(parseInt(argument))
+}else{
+    console.log("Command not found !");
+}
